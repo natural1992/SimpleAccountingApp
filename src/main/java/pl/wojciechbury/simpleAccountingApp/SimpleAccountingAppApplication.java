5ea0cbb0880a07b0a36e71f67bf8794086ba87e5
Wojciech Bury <bury1992@gmail.com>
2018-11-16 13:58:52 +0100

    Initial commit

---
+++ src/main/java/pl/wojciechbury/simpleAccountingApp/SimpleAccountingAppApplication.java
@@ -0,0 +1,12 @@
+package pl.wojciechbury.simpleAccountingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAccountingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAccountingAppApplication.class, args);
+	}
+}
